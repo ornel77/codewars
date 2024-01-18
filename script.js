58213ae99909c00bb8330bc8d106be3f71bc7937
ornel77 <t.ornel@yahoo.fr>
2024-01-18 18:12:06 +0100

    remove function

--- script.js
+++ script.js
@@ -342,53 +342,6 @@ function moveZeros(arr) {
 // console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]))
 
 /* ----------------------------------- ** ----------------------------------- */
-function zero() {
-  return 0;
-}
-function one() {
-  return 1;
-}
-function two() {
-  return 2;
-}
-function three() {
-  return 3;
-}
-function four() {
-  return 4;
-}
-function five() {
-  return 5;
-}
-function six() {
-  return 6;
-}
-function seven() {
-  return 7;
-}
-function eight() {
-  return 8;
-}
-function nine() {
-  return 9;
-}
-
-function plus() {
-  return "+";
-}
-function minus() {
-  return "-";
-}
-function times() {
-  return "*";
-}
-function dividedBy() {
-  return "/";
-}
-
-// console.log(eval(seven(times(five()))));
-
-/* ----------------------------------- ** ----------------------------------- */
 function monkeyCount(n) {
   // your code here
     // return [...Array(n).keys()]
@@ -397,4 +350,4 @@ function monkeyCount(n) {
     return [...Array(26)].map((_,i) => (i+10).toString(16))
 
   }
-  console.log(monkeyCount(1))
+  // console.log(monkeyCount(1))

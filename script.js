01db9ec8ff2dee01b93f778bb6026a4e66cb090e
ornel77 <ornella.tomboza@gmail.com>
2024-01-29 09:39:32 +0100

    first commit of the day 29012024

--- script.js
+++ script.js
@@ -10,4 +10,44 @@ function finalGrade (exam, projects) {
   }
 }
 
-console.log(finalGrade(100, 12))
+// console.log(finalGrade(100, 12))
+
+/* ------------------------- Moving Zeros To The End ------------------------ */
+function moveZeros(arr) {
+  let zeros = arr.filter(elmt => elmt === 0 && elmt !== null)
+  let final = arr.filter(elmt => elmt !== 0)
+  final.push(zeros)
+  // console.log(zeros, final)
+  return final.flat()
+
+}
+
+// console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
+// console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]))
+// console.log(moveZeros([ 'a', 'b', null, 'c', 'd', 1, false, 1, 3, [], 1, 9, {}, 9, +0, +0, +0, +0, +0, +0, +0, +0, +0, +0 ]))
+
+/* ------------------------- Stop gninnipS My sdroW! ------------------------ */
+function spinWords(string){
+  let ar = string.split(' ')
+  let final = []
+  ar.forEach(word => {
+    if(word.length >= 5) {
+      final.push(word.split('').reverse().join(''))
+    } else {
+      final.push(word)
+    }
+  });
+
+  return final.join(' ')
+}
+
+/* --------------------------- others solutions --------------------------- */
+function spinWords(words){
+  return words.split(' ').map(function (word) {
+    return (word.length > 4) ? word.split('').reverse().join('') : word;
+  }).join(' ');
+}
+function spinWords(string){
+  return string.replace(/\w{5,}/g, function(w) { return w.split('').reverse().join('') })
+}
+console.log(spinWords("Hey fellow warriors"))
